e59bc7fc27d4b035643ccf77d815b5763196a4b3
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-01 18:43:07 +0900

    fix

--- typical90/src/bin/001.rs
+++ typical90/src/bin/001.rs
@@ -1,5 +1,14 @@
 use proconio::input;
 
 fn main() {
-    todo!();
+    input! {
+    n:usize,
+    l:i32,
+    k:i32,
+    a:[i32; n],
+    }
+
+    let mut ans_list = Vec::new();
+
+    for i in 0..n {}
 }
