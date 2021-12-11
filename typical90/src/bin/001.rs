2ea21270a8d51f8ecea0d8fbbb589d1d6fcd07ce
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-12 03:03:05 +0900

    start typical90

--- typical90/src/bin/001.rs
+++ typical90/src/bin/001.rs
@@ -2,5 +2,34 @@ use proconio::*;
 
 #[fastout]
 fn main() {
-    input!{}
+    input! {
+        n:usize,
+        l:i32,
+        k:i32,
+        mut a:[i32; n],
+    }
+    a.sort();
+    let mut ok = 0;
+    let mut ng = l + 1;
+    while 1 < (ng - ok).abs() {
+        let mid = (ok + ng) / 2;
+        if solve(l, k, &a, mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    println!("{}", ok);
+}
+
+fn solve(l: i32, k: i32, a: &Vec<i32>, m: i32) -> bool {
+    let mut cnt = 0;
+    let mut pre = 0;
+    for i in a {
+        if i - pre >= m && m <= l - i {
+            pre = *i;
+            cnt += 1;
+        }
+    }
+    k <= cnt
 }
