37eda5b6f7427554d2422cd5a7c3d972510ff809
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-22 09:44:51 +0900

    understand abc230 c

--- abc230/src/bin/c.rs
+++ abc230/src/bin/c.rs
@@ -1,5 +1,27 @@
-use proconio::input;
+use proconio::marker::{Bytes, Chars};
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
-    input!{}
+    input! {
+        n: i64,
+        a: i64,
+        b: i64,
+        p: i64,
+        q: i64,
+        r: i64,
+        s: i64,
+    }
+
+    for i in p..=q {
+        let mut ans = String::new();
+        for j in r..=s {
+            if (i - j == a - b) || (i + j == a + b) {
+                ans += "#";
+            } else {
+                ans += ".";
+            }
+        }
+        println!("{}", ans);
+    }
 }
