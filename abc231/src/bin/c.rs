5b954d3e8e3dfa201daa32f7e683a332a682e9c7
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2021-12-22 15:53:11 +0900

    understand abc232 c

--- abc231/src/bin/c.rs
+++ abc231/src/bin/c.rs
@@ -12,28 +12,3 @@ fn main() {
         println!("{}", n - a.binary_search(&xi).unwrap_or_else(|i| i));
     }
 }
-
-// use proconio::input;
-// fn main() {
-//     input! {
-//         n:usize,
-//         q:usize,
-//         mut a: [i32; n],
-//         mut x: [i32; q],
-//     }
-//     let mut ans = Vec::new();
-
-//     for i in 0..q {
-//         let mut tmp = 0;
-//         for j in &a{
-//             if x[i] <= *j {
-//                 tmp += 1;
-//             }
-//         }
-//         ans.push(tmp);
-//     }
-
-//     for k in 0..q {
-//         println!("{}", ans[k as usize]);
-//     }
-// }
--- abc232/src/bin/c.rs
+++ abc232/src/bin/c.rs
@@ -1,31 +1,33 @@
+use proconio::marker::Usize1;
 use proconio::*;
+use superslice::Ext;
 
 #[fastout]
 fn main() {
     input! {
-        n:usize,
-        m:usize,
-        mut mat: [[usize; 2]; m * 2],
+        n: usize,
+        m: usize,
+        mut ab: [(Usize1, Usize1); m],
+        mut cd: [(Usize1, Usize1); m],
     }
-    let mut a = Vec::new();
-    let mut b = Vec::new();
-    let mut c = Vec::new();
-    let mut d = Vec::new();
-    for i in 0..m{
-        a.push(mat[i][0]);
-        b.push(mat[i][1]);
-    }
-    for i in 0..m{
-        c.push(mat[i + m][0]);
-        d.push(mat[i + m][1]);
-    }
-    let mut flag = false;
-
-    
-
-    if flag {
+    ab.sort();
+    cd.sort();
+    if ab == cd {
         println!("Yes");
-    } else {
-        println!("No");
+        return ();
     }
+
+    let mut p = (0..n).collect::<Vec<_>>();
+    while p.next_permutation() {
+        let mut tmp = Vec::new();
+        for &(c, d) in &cd {
+            tmp.push((p[c].min(p[d]), p[c].max(p[d])));
+        }
+        tmp.sort();
+        if ab == tmp {
+            println!("Yes");
+            return ();
+        }
+    }
+    println!("No");
 }
