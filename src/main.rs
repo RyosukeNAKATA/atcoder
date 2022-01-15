8e252fba97ac97075c1cdde49846cc304679b3a8
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-01-15 22:45:37 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 // A
-// use proconio::marker::{Bytes, Chars};
-// use proconio::{fastout,input};
+use proconio::marker::{Bytes, Chars};
+use proconio::{fastout,input};
 
-// #[fastout]
-// fn main() {
-//     input! {
-//         abc: Bytes
-//     }
-//     let a = abc[0] as i32 -48;
-//     let b = abc[1] as i32 -48;
-//     let c = abc[2] as i32 -48;
-//     let abc = 100 * a + 10 * b + c;
-//     let bca = 100 * b + 10 * c + a;
-//     let cba = 100 * c + 10 * a + b;
-//     println!("{}",abc+bca+cba);
-// }
+#[fastout]
+fn main() {
+    input! {
+        abc: Bytes
+    }
+    let a = abc[0] as i32 -48;
+    let b = abc[1] as i32 -48;
+    let c = abc[2] as i32 -48;
+    let abc = 100 * a + 10 * b + c;
+    let bca = 100 * b + 10 * c + a;
+    let cba = 100 * c + 10 * a + b;
+    println!("{}",abc+bca+cba);
+}
 
 // B
 // use proconio::marker::{Bytes, Chars};
