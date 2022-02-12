ac3d5cd7c98c5ae4c09796fbedb5304553ebf8e8
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-02-12 23:14:10 +0900

    resolve typical90 004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,7 +1,32 @@
-use proconio::{fastout,input};
+use std::usize;
+
 use proconio::marker::{Bytes, Chars};
+use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input!{}
+    input! {
+        h: usize,
+        w: usize,
+        mat_a: [[u32; w]; h],
+    }
+
+    let mut row = vec![0; h];
+    let mut column = vec![0; w];
+    for i in 0..h {
+        for j in 0..w {
+            row[i] += mat_a[i][j];
+            column[j] += mat_a[i][j];
+        }
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            print!("{}", row[i] + column[j] - mat_a[i][j]);
+            if j != w - 1 {
+                print!(" ");
+            }
+        }
+        println!("");
+    }
 }
