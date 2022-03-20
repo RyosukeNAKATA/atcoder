7d703940c276a16980f9ac744ed2169688e84b57
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-03-20 23:35:53 +0900

    learn abc244/c

--- abc244/src/bin/c.rs
+++ abc244/src/bin/c.rs
@@ -1,27 +1,27 @@
-use proconio::marker::{Bytes, Chars, Usize1};
-use proconio::{fastout, input};
+use std::collections::HashSet;
 use std::io;
 use std::io::Write;
-#[fastout]
+use std::process::exit;
 fn main() {
-    input! {
-        n: usize,
-    }
-    let mut num_list = vec![];
-    for i in 0..2 * n + 1 {
-        num_list.push((i + 1) as i32)
-    }
+    let n: usize = read();
 
-    for _ in 0..n + 1 {
-        println!("{}", num_list[0]);
-        io::stdout().flush().unwrap();
-        input! {
-            a: i32
-        }
-        if a == 0 {
-            return;
-        } else {
-            num_list.remove(num_list.binary_search(&a).unwrap());
-        }
+    let mut set = (1..=((2 * n) + 1)).collect::<HashSet<_>>();
+    let mut now: usize = 1_usize;
+
+    loop {
+        let now = set.iter().next().unwrap().to_owned();
+        println!("{}", set.take(&now).unwrap());
+        std::io::stdout().flush().ok();
+        let a = read();
+        match a {
+            0 => exit(0),
+            an => set.take(&an),
+        };
     }
+}
+
+fn read() -> usize {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().unwrap()
 }
