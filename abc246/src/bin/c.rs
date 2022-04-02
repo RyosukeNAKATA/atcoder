9f9a02f48167a4ebfa3c7650a92de7c2d0a22114
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-04-02 22:57:57 +0900

    learn abc246 c use 64!

--- abc246/src/bin/c.rs
+++ abc246/src/bin/c.rs
@@ -1,45 +1,29 @@
-use proconio::marker::{Bytes, Chars, Usize1};
+use proconio::marker::{Bytes, Chars};
 use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        mut k: i32,
-        x: i32,
-        mut a: [i32; n],
+        n: i64,
+        k: i64,
+        x: i64,
+        mut a: [i64; n],
     }
     a.sort();
     a.reverse();
 
-    for i in 0..a.len() {
-        if a[i] < x {
-            continue;
-        } else {
-            let tmp = k;
-            'inside: for _ in 0..tmp {
-                if k == 0 || a[i] < x {
-                    break 'inside;
-                } else {
-                    a[i] += -x;
-                    k += -1;
-                }
-            }
-        }
+    let mut tmp = k;
+    for i in 0..n {
+        let y = tmp.min(a[i as usize] / x);
+        tmp -= y;
+        a[i as usize] -= x * y;
     }
 
-    let mut i = 0;
-    while k > 0 {
-        a[i] -= x;
-        if a[i] < 0 {
-            a[i] = 0;
-        }
-        k += -1;
-        i += 1;
-        if i == a.len() {
-            break;
-        }
+    a.sort();
+    a.reverse();
+    for i in 0..tmp.min(n) {
+        a[i as usize] = 0;
     }
 
-    let answer: i32 = a.iter().sum();
+    let answer = a.iter().sum::<i64>();
     println!("{}", answer);
 }
