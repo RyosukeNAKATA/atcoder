d9109d4daf72ea99a792ee729bf0aed9d6de1d3f
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-04-10 23:04:14 +0900

    learn abc247 d Run-Length Method

--- abc247/src/bin/d.rs
+++ abc247/src/bin/d.rs
@@ -1,26 +1,41 @@
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
-use std::io;
+use std::collections::VecDeque;
+
 #[fastout]
 fn main() {
     input! {
         q: usize,
     }
-    let mut queries = vec![];
+
+    let mut vecdeq = VecDeque::new();
 
     for _ in 0..q {
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).unwrap();
-        println!("{:?}", s);
-        let tmp: Vec<i64> = s
-            .trim()
-            .split_whitespace()
-            .map(|e| e.parse().ok().unwrap())
-            .collect();
-        queries.push(tmp);
+        input! {z: usize}
+        if z == 1 {
+            input! {
+                x: usize,
+                c: usize,
+            }
+            vecdeq.push_back((x, c));
+        } else {
+            input! {
+                mut c: usize,
+            }
+
+            let mut sum = 0;
+            while c > 0 {
+                let (x, k) = vecdeq.pop_front().unwrap();
+                if k < c {
+                    sum += x * k;
+                    c -= k;
+                } else {
+                    vecdeq.push_front((x, k - c));
+                    sum += x * c;
+                    c = 0;
+                }
+            }
+            println!("{}", sum);
+        }
     }
-    println!("{:?}", queries);
-    //for x in queries {
-    //    println!("{:?}", x);
-    //}
 }
