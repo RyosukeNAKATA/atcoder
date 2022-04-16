09f2c0dc4b5e5f70a9673c46988e5292749ed395
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2022-04-16 23:00:21 +0900

    learn abc247 c dp!

--- abc248/src/bin/c.rs
+++ abc248/src/bin/c.rs
@@ -7,11 +7,19 @@ fn main() {
         m: usize,
         k: usize,
     }
-    let num_check = 998244353;
+    const MOD: u64 = 998244353;
 
-    let mut dp = vec![vec![0; m + 1]; n + 1];
-    for i in 0..n + 1 {
-        dp[i][0] = 1
+    let mut dp = vec![vec![0; k + 1]; n + 1];
+    dp[0][0] = 1;
+    for i in 0..n {
+        for j in 0..=k {
+            for ai in 1..=m {
+                if j + ai <= k {
+                    dp[i + 1][j + ai] += dp[i][j];
+                    dp[i + 1][j + ai] %= MOD;
+                }
+            }
+        }
     }
-    println!("{}", dp[n][n]);
+    println!("{}", dp[n].iter().fold(0, |s, c| (s + c) % MOD));
 }
